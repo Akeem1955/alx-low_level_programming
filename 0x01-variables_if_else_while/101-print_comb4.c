f1f5eef61cf96c020e5542245dfc38a00f05c6e6
Akeem1955 <adetunjiadams29@gmail.com>
2022-10-15 08:42:43 -0700

    cisfub

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,29 +6,23 @@
  */
 int main(void)
 {
-	int i, j, k, l;
+	int i, j, k;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i < 57; i++)
 	{
 		for (j = 48; j < 57; j++)
 		{
-			putchar(i);
-			putchar(j);
-			putchar(' ');
 			for (k = 48; k <= 57; k++)
 			{
-				for (l = 48; l <= 57; l++)
+				if (j > i && k > j)
 				{
-					if ((k >= i && l > j) && (i != 57 || j != 56))
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56 || k != 57)
 					{
-						putchar(k);
-						putchar(l);
 						putchar(',');
 						putchar(' ');
-					} else
-					{
-						putchar(k);
-						putchar(l);
 					}
 				}
 			}
