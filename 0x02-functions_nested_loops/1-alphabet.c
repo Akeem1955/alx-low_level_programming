62117ef0892bb60c7211c0c33d8c2aa3a64323a7
Akeem1955 <adetunjiadams29@gmail.com>
2022-10-18 04:50:41 -0700

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
  * print_alphabet - print all alphabet in small letter
- *
+ * Return:0
  */
 
 void print_alphabet(void)
 {
 	for (int k = 'a'; k <= 'z'; k++)
+	{
 		_putchar(k);
+	}
 	_putchar('\n');
 }
