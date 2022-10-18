dc830fae9d187a280a18ac6a623255131db0c25a
Akeem1955 <adetunjiadams29@gmail.com>
2022-10-18 05:49:29 -0700

    cisfun

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,7 @@ int print_last_digit(int l)
 {
 	if (l % 10 < 0)
 	{
-		putchar(((1 % 10) * -(1)) + 48)
+		_putchar(((1 % 10) * -(1)) + 48)
 		return ((l % 10) * -(1));
 	}
 	_putchar((1 % 10) + 48)
