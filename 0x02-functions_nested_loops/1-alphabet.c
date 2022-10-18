8ba1169613845543d5a856d2b74c54c2ba254828
Akeem1955 <adetunjiadams29@gmail.com>
2022-10-17 23:15:31 -0700

    cisfun

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in small letter
+ *
+ */
+
+void print_alphabet(void)
+{
+	for (int k = 'a'; k <= 'z'; k++)
+		_putchar(k);
+	_putchar('\n');
+}
