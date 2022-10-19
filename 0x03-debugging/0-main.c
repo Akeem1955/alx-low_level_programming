7af6a8fdec731e052ee2134f3db7572483992b3c
Akeem1955 <adetunjiadams29@gmail.com>
2022-10-19 08:44:09 -0700

    cisfun

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
  * main - test function that print if integer is positive or negative
- * Return:0
+ * Return: 0
  */
 
 int main(void)
@@ -9,7 +10,7 @@ int main(void)
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
+
 	return (0);
 }
