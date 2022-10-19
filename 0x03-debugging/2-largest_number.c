27d6d6fd2545381d677b48d4d6b55e1e01bdeeea
Akeem1955 <adetunjiadams29@gmail.com>
2022-10-19 08:31:18 -0700

    cisfun

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,11 +14,11 @@ int largest_number(int a, int b, int c)
 
 	largest = a;
 
-	if (b > a && b > c)
+	if (b >= a && b >= c)
 	{
 		largest = b;
 	}
-	if(c > a && c > b)
+	if (c >= a && c >= b)
 	{
 		largest = c;
 	}
