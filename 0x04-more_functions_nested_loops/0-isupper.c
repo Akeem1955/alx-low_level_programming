64e8f91e6160ca931e79dd9763a00abbad5a979c
Akeem1955 <adetunjiadams29@gmail.com>
2022-10-20 09:29:27 -0700

    cisfun

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppeercase character
+ * @c:charcater to check for uppercase
+ * Return:1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
