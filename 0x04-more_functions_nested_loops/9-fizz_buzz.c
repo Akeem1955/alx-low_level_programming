e104e8fad3bb164a5eb9620cbc4f668d8f38dc29
Akeem1955 <adetunjiadams29@gmail.com>
2022-10-20 17:41:13 -0700

    cisfun

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
@@ -27,6 +27,11 @@ int main(void)
 		{
 			printf("%d", i);
 		}
+		if  (i < 100)
+		{
+			printf(" ");
+		}
 	}
+	printf("\n");
 	return (0);
 }
