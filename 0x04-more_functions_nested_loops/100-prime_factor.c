90de15d78515f24acb0b49f8895a860741628c63
Akeem1955 <adetunjiadams29@gmail.com>
2022-10-21 13:28:03 -0700

    cisfun

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,30 @@
-#include "main.h"
+#include<stdio.h>
+#include<math.h>
 
 /**
- * print_number
+ * main - largest prime
+ * Return:0
+ */
+
+int main(void)
+{
+	unsigned long int n, i;
+
+	n = 612852475143;
+	while (n % 2 ==  0)
+	{
+		n = n / 2;
+	}
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			n = n / i;
+		}
+	}
+	if (n > 2)
+	{
+		printf("%lu\n", n);
+	}
+	return (0);
+}
