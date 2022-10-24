8f2cb4b63924ce450def4ee8a1121bf49664044c
Akeem1955 <adetunjiadams29@gmail.com>
2022-10-24 08:47:37 -0700

    CISFUN

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,11 +4,14 @@
  * _strcpy - copy a string
  * @dest: string to copy to
  * @src:source to copy from
+ * Return:copy of src
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+
+	i = 0;
 	while (src[i])
 	{
 		dest[i] = src[i];
