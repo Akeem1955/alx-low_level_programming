a2cf90e3c5b6d5ea5bb3fefc4366abd6f16a6fc8
Akeem1955 <adetunjiadams29@gmail.com>
2022-10-24 04:19:42 -0700

    CISFUN

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string, starting with the first character, followed by a new line.
+ * @str:String to print
+ * Return:0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(s[i])
+		}
+	}
+	_putchar('\n');
+}
