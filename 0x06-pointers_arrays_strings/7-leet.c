87222803f96beca13c7605224ce3785088cb3e84
Akeem1955 <adetunjiadams29@gmail.com>
2022-10-27 09:09:41 -0700

    CISFUN

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - appends to a string
+ * @p:string
+ * Return:char pointer
+ */
+
+char *leet(char *p)
+{
+	int i, j;
+	char encodeA[]="aAeEoOtTlL";
+	char encodeB[]="4433007711";
+
+	i = 0;
+	while (p[i])
+	{
+		j = 0;
+		while (p[i] != encodeA[j] && j < 10)
+		{
+			j++;
+		}
+		
+		if (p[i] == encodeA[j])
+		{
+			p[i] =encodeB[j];
+		}
+		i++;
+	}
+	return (&p[0]);
+}
