40c89920a9f4415d43043843fccb1b7542f202e3
Akeem1955 <adetunjiadams29@gmail.com>
2022-10-27 02:39:01 -0700

    cisfun

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -5,10 +5,8 @@ int main(void)
 {
 	char s1[98] = "Hello ";
 	char s2[] = "World!\n";
-	char *ptr;
 
-	ptr = _strncpy(s1, s2, 1);
-	ptr = _strncpy(s1, s2, 1024);
-	printf("%s\n", ptr);
+
+	printf("%d\n", _strcmp(s1, s2));
 	return (0);
 }
