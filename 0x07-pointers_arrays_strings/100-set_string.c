2b31fcf7b376a1976aecdbd82665894893c2f3be
Akeem1955 <adetunjiadams29@gmail.com>
2022-11-03 06:59:07 -0700

    cisfun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * set_string - print chess
+ * @s:poinrt to the matrix
+ * @to:size of array
+ * Return:0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
