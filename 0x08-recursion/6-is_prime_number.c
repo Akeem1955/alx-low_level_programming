52724f0a0c94703251878456e5ea3224ad57edef
Akeem1955 <adetunjiadams29@gmail.com>
2022-11-04 00:55:39 -0700

    cisfun

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -29,9 +29,9 @@ int Prime(int a, int b)
 }
 
 /**
- * _sqrt_recursion - return sqrt
+ * is_prime_number - return 0 or 1
  * @n:parameter to check
- * Return:sqrt root
+ * Return:0 or 1
  */
 
 int is_prime_number(int n)
