3380f646a4c7a1a50484c6de5282eb4711553b33
Akeem1955 <adetunjiadams29@gmail.com>
2022-11-05 01:14:17 -0700

    cisfun

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,10 +9,7 @@
 
 int main(int argc, char* argv[])
 {
-	int i;
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n",argv[i]);
-	}
-	return (0);
+	printf("%d\n", argc - 1);
+
+	return (sizeof(argv[0]));
 }
